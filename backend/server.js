3598d78e1fac5a15dab6d10b64257b32f76cb776
Tiago Pita <tiagomsp18@ua.pt>
2025-06-04 21:30:58 +0100

    Add README

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -8,6 +9,6 @@ app.use(express.json());
 const authRoutes = require('./routes/auth');
 app.use('/api', authRoutes);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Servidor a correr na porta 3000');
 });
